c6c4dc55a83dfcd1df5e0c04c1817fdedd1c6429
ChesnokovEK <4echoko@gmail.com>
2024-08-29 17:09:53 +0300

    fix: ТЗ-12. Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dao/Impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/Impl/FilmDbStorage.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
-import java.time.LocalDate;
 import java.util.*;
 
 @Component
