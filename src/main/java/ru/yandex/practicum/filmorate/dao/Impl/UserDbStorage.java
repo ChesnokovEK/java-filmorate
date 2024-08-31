6ee4bd52ae982c9f070b74ad37cc76eae45598bf
ChesnokovEK <4echoko@gmail.com>
2024-08-31 15:37:44 +0300

    fix: ТЗ-12. Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dao/Impl/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/Impl/UserDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dao.Impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
